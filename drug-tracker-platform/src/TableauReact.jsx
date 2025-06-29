71274a08e0ad06add01a4db3e25b0793dfbdec38
Kosi <kosiaroh@gmail.com>
2025-06-29 18:28:22 -0400

    add theme with dashboard

---
+++ drug-tracker-platform/src/TableauReact.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import tableau from 'tableau-api';
+
+var viz
+
+class TableauReact extends Component {
+  componentDidMount() {
+    this.initTableau()
+  }
+ 
+  initTableau() {
+    const vizUrl = 'https://public.tableau.com/views/ShortageTrackerDashboard/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link';
+    const vizContainer = this.vizContainer;
+    if(viz){
+        viz.dispose();
+    }
+    viz = new window.tableau.Viz(vizContainer, vizUrl)
+  }
+ 
+  render() {
+    return (
+      <div ref={(div) => { this.vizContainer = div }}>
+      </div>
+    )
+  }
+}
+ 
+export default TableauReact;
